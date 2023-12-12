403c52cb080dd690946ba9f1f7ed9f3d9fd0673e
JackSeraph999 <150169040+JackSeraph999@users.noreply.github.com>
2023-12-12 11:02:31 -0500

    Add files via upload

---
+++ P2HW2_FISHERLATRAVIS(JACK).py
@@ -0,0 +1,29 @@
+#CTI-110
+#P2HW2-List
+#FISHER LATRAVIS(JACK)
+#11/17/202
+
+Module1 = float(input("Enter grade for Module1: "))
+Module2 = float(input("Enter grade for Module2: "))
+Module3 = float(input("Enter grade for Module3: "))
+Module4 = float(input("Enter grade for Module4: "))
+Module5 = float(input("Enter grade for Module5: "))
+Module6 = float(input("Enter grade for Module6: "))
+
+Grades = [65.5,88,78.5,90,61,92]
+
+Sum = sum(Grades)
+print(Sum)
+
+Low_Grade = min(Grades)
+print(Low_Grade)
+
+Max = max(Grades)
+print(Max)
+
+Avg = sum(Grades) / len(Grades)
+print(Avg)
+
+
+
+
